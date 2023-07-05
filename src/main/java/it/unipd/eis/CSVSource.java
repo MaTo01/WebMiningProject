d9edddc15ff32b2332e150d6620afe13404ecade
Van Sang Ho <vansang.hocorp@gmail.com>
2023-07-05 18:15:14 +0200

    lettura da CSV tramite libreria invece delle super cagate che avevo fatto prima

--- src/main/java/it/unipd/eis/CSVSource.java
+++ src/main/java/it/unipd/eis/CSVSource.java
@@ -1,6 +1,7 @@
 package it.unipd.eis;
 
-import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -16,14 +17,14 @@ public class CSVSource implements Source {
     @Override
     public void deserializeArticles() {
         try (BufferedReader reader = new BufferedReader(new FileReader("dove minchia mettiamo i file csv"))) {
-            String fieldList = reader.readLine().toLowerCase();
-            int titleIndex = ArrayUtils.indexOf(fieldList.split(","), "title");
-
             FileArticleStorage fas = new FileArticleStorage("Storage");
-            String line;
-            while((line = reader.readLine()) != null) {
-                String title = line.split(",")[titleIndex];
-                String bodyText = line.split("\"")[1];
+
+            Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(reader);
+
+            for (CSVRecord record : records) {
+                String title = record.get("Title");
+                String bodyText = record.get("Body");
+
                 fas.addArticle(new Article(
                         title,
                         bodyText
