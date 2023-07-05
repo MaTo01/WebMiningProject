feba36852e272d4544f4b6f89916881a3b6964f5
Van Sang Ho <vansang.hocorp@gmail.com>
2023-07-05 18:51:27 +0200

    commenti per Javadoc

--- src/main/java/it/unipd/eis/Source.java
+++ src/main/java/it/unipd/eis/Source.java
@@ -1,6 +1,16 @@
 package it.unipd.eis;
 
 public interface Source {
+    /**
+     * Downloads articles from the source.
+     *
+     * @param query the term(s) used for the article search
+     */
     void downloadArticles(String query);
-    void deserializeArticles();
+
+    /**
+     * Saves the articles to a storage.
+     * @throws IllegalStateException if called before articles are obtained through downloadArticles
+     */
+    void serializeArticles() throws IllegalStateException;
 }
