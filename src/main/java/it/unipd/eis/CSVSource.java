c23ccdaef6e09bc37bbd512bb09ee91f4973d25a
Van Sang Ho <vansang.hocorp@gmail.com>
2023-07-05 17:35:21 +0200

    metodi CSVSource

--- src/main/java/it/unipd/eis/CSVSource.java
+++ src/main/java/it/unipd/eis/CSVSource.java
@@ -1,4 +1,36 @@
 package it.unipd.eis;
 
-public class CSVSource implements Source{
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CSVSource implements Source {
+
+    @Override
+    public void downloadArticles(String keyword) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void deserializeArticles() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("dove minchia mettiamo i file csv"))) {
+            String fieldList = reader.readLine().toLowerCase();
+            int titleIndex = ArrayUtils.indexOf(fieldList.split(","), "title");
+
+            String line;
+            FileArticleStorage fas = new FileArticleStorage("Storage");
+            while((line = reader.readLine()) != null) {
+                String title = line.split(",")[titleIndex];
+                String bodyText = line.split("\"")[1];
+                fas.addArticle(new it.unipd.eis.Article(
+                        title,
+                        bodyText
+                ));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
