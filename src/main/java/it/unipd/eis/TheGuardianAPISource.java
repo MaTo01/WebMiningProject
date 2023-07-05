2284dbc357dc0008a838ceb76a9e813878657b78
Van Sang Ho <vansang.hocorp@gmail.com>
2023-07-05 17:35:06 +0200

    metodi GuardianApiSource

--- src/main/java/it/unipd/eis/TheGuardianAPISource.java
+++ src/main/java/it/unipd/eis/TheGuardianAPISource.java
@@ -1,4 +1,47 @@
 package it.unipd.eis;
 
-public class TheGuardianAPISource implements Source{
+import com.apiguardian.*;
+import com.apiguardian.bean.Article;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TheGuardianAPISource implements Source {
+
+    private com.apiguardian.bean.Article[] responseArticles;
+    private String apiKey;
+
+    public TheGuardianAPISource() {
+        getApiKey();
+    }
+    @Override
+    public void downloadArticles(String query) {
+        GuardianContentApi guardianApi = new GuardianContentApi(apiKey);
+        try {
+            responseArticles = guardianApi.getContent(query).getResults();
+        } catch(UnirestException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void deserializeArticles() {
+        FileArticleStorage fas = new FileArticleStorage("Storage");
+        for (com.apiguardian.bean.Article a : responseArticles) {
+            fas.addArticle(new it.unipd.eis.Article(
+                    a.getWebTitle(),
+                    a.getBodyText()
+            ));
+        }
+    }
+
+    private void getApiKey() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("Storage/guardianApiKey.txt"))) {
+            apiKey = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
