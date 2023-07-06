ebea27897129a02c8d8761abe3c957e6dd4e9b8e
Van Sang Ho <vansang.hocorp@gmail.com>
2023-07-06 12:47:51 +0200

    parziale Main con uso di Apache Commons CLI

--- src/main/java/it/unipd/eis/App.java
+++ src/main/java/it/unipd/eis/App.java
@@ -1,13 +1,28 @@
 package it.unipd.eis;
 
-/**
- * Hello world!
- *
- */
+import org.apache.commons.cli.*;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        Options options = new Options();
+        options.addOption("d", true, "Download articles based on a key word/phrase");
+        options.addOption("e", false, "Extract the terms with the highest weight");
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = null;
+        try {
+            cmd = parser.parse(options, args);
+
+            if(cmd.hasOption("d")) {
+                TheGuardianAPISource guardianSource = new TheGuardianAPISource();
+                CSVSource csvSource = new CSVSource();
+
+                //controllo su valore null
+                guardianSource.downloadArticles(cmd.getOptionValue("d"));
+            }
+        } catch(ParseException pe) {
+
+        }
     }
 }
