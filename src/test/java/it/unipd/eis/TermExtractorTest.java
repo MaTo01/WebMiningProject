5e82dd02c51a76cd405ccb0f3000fa54e50c0423
Van Sang Ho <vansang.hocorp@gmail.com>
2023-07-07 16:11:07 +0200

    classe TermExtractorTest

---
+++ src/test/java/it/unipd/eis/TermExtractorTest.java
@@ -0,0 +1,11 @@
+package it.unipd.eis;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class TermExtractorTest {
+    private TermExtractor extractor;
+    @BeforeEach
+    public void setup() {
+        extractor = new TermExtractor();
+    }
+}
