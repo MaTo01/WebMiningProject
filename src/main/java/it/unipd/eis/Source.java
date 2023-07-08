2eccfeaa9d2b4e5fe0ac378c4fa5a771a6958e6c
Van Sang Ho <vansang.hocorp@gmail.com>
2023-07-08 12:07:01 +0200

    Javadoc + spostato un'inizializzazione

--- src/main/java/it/unipd/eis/Source.java
+++ src/main/java/it/unipd/eis/Source.java
@@ -2,21 +2,36 @@ package it.unipd.eis;
 
 import java.util.ArrayList;
 
+/**
+ * Abstract class representing different types of sources.
+ */
 public abstract class Source {
-    protected ArrayList<Article> articles = new ArrayList<>();
-    private static ArticleStorage storage;
+    protected ArrayList<Article> articles;
+    private final static ArticleStorage storage = new FileArticleStorage("Storage");
 
     public Source() {
-        storage = new FileArticleStorage("Storage");
+        articles = new ArrayList<>();
     }
 
+    /**
+     * Downloads articles from a source that doesn't require a search query (e.g. CSV file).
+     */
     abstract public void downloadArticles();
+    /**
+     * Downloads articles from a source that requires a search query (e.g. TheGuardian API).
+     */
     abstract public void downloadArticles(String query);
 
+    /**
+     * Serializes the downloaded articles.
+     */
     protected void serializeArticles() {
         storage.addArticles(articles);
     }
 
+    /**
+     * Clears all the stored articles.
+     */
     public static void clearStorage() {
         storage.clearStorage();
     }
