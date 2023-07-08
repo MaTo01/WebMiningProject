371a59011ddaf9ab9eb2f0e5a314490266d446fb
Van Sang Ho <vansang.hocorp@gmail.com>
2023-07-08 12:07:34 +0200

    Javadoc + reso numero di articoli da scaricare modificabile e aggiustato codice di conseguenza

--- src/main/java/it/unipd/eis/TheGuardianAPISource.java
+++ src/main/java/it/unipd/eis/TheGuardianAPISource.java
@@ -8,37 +8,68 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+/**
+ * Source used to download and serialize articles from TheGuardian API.
+ */
 public class TheGuardianAPISource extends Source {
-    private static String apiKey;
-    private static final String apiKeyFilePath = "Storage/TheGuardianAPIKey.txt";
+    private String apiKey;
+    private String apiKeyFilePath = "Storage/TheGuardianAPIKey.txt";
     private static final int pageSize = 200;
-    private static final int numArticles = 1000;
+    private int numArticles = 1000;
 
+    /**
+     * Constructs a TheGuardianAPISource using the default API key file path.
+     */
     public TheGuardianAPISource() {
         getApiKey();
     }
 
+    /**
+     * Constructs a TheGuardianAPISource using the specified API key file path.
+     * @param keyPath the file path of the API key
+     */
+    public TheGuardianAPISource(String keyPath) {
+        apiKeyFilePath = keyPath;
+        getApiKey();
+    }
+
+    /**
+     * Sets the number of articles to be downloaded.
+     * @param numArticles number of articles to download
+     */
+    public void setNumArticles(int numArticles) {
+        this.numArticles = numArticles;
+    }
+
+    /**
+     * The implementation of downloadArticles() without a search query is disabled for this Source.
+     */
     public void downloadArticles() {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Queries TheGuardian API using the specified search string to obtain articles.
+     * @param query the String to use as keyword for the search
+     */
     @Override
     public void downloadArticles(String query) {
         GuardianContentApi guardianApi = new GuardianContentApi(apiKey);
         guardianApi.setPageSize(pageSize);
 
         try {
-            for (int i = 1; i <= numArticles/pageSize; i++) {
-                guardianApi.setPage(i);
+            int pages = (int) Math.ceil((double) numArticles / pageSize);
+            for (int i = 1; i <= pages; i++) {
                 com.apiguardian.bean.Response response = guardianApi.getContent(query);
-                if(response.getStatus().equals("ok")) {
 
-                    int k = response.getCurrentPage();
+                if(response.getStatus().equals("ok")) {
+                    int left = numArticles - (i * pageSize);
                     com.apiguardian.bean.Article[] responseArticles = response.getResults();
-                    for (com.apiguardian.bean.Article a : responseArticles) {
+
+                    for (int j = 0; j < responseArticles.length && j < left; j++) {
                         articles.add(new Article(
-                                a.getWebTitle(),
-                                a.getBodyText()
+                                responseArticles[j].getWebTitle(),
+                                responseArticles[j].getBodyText()
                         ));
                     }
                 } else {
@@ -52,6 +83,9 @@ public class TheGuardianAPISource extends Source {
         }
     }
 
+    /**
+     * Loads the API key from the previously specified file.
+     */
     private void getApiKey() {
         try (BufferedReader reader = new BufferedReader(new FileReader(apiKeyFilePath))) {
             apiKey = reader.readLine();
