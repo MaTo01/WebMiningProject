f0da2334bdaf56e07e213a1fa09b2e65aff469ef
Van Sang Ho <vansang.hocorp@gmail.com>
2023-07-10 11:37:23 +0200

    test CSVSource

--- src/test/java/it/unipd/eis/CSVSourceTest.java
+++ src/test/java/it/unipd/eis/CSVSourceTest.java
@@ -3,17 +3,24 @@ package it.unipd.eis;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CSVSourceTest {
-    private static final String FILE_PATH = "./Storage";
-    private Source source;
+    private static ArticleStorage articleStorage = new FileArticleStorage("./Storage/Test");
+    private static final String sourceName = "nytimes_articles_v2.csv";
+    private CSVSource source;
 
     @BeforeEach
     public void setup() {
-        source = new CSVSource();
+        CSVSource.setStorage(articleStorage);
+        source = new CSVSource(sourceName);
     }
 
     @Test
     public void testDownloadArticles() {
+        CSVSource.clearStorage();
+        source.downloadArticles();
 
+        assertEquals(1000, articleStorage.getArticleCount());
     }
 }
