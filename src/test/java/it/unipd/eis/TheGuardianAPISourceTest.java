32c8ee5a8fc023a5c2f97f9581910bbc7caf672b
Van Sang Ho <vansang.hocorp@gmail.com>
2023-07-10 11:37:47 +0200

    test TheGuardianAPISource

--- src/test/java/it/unipd/eis/TheGuardianAPISourceTest.java
+++ src/test/java/it/unipd/eis/TheGuardianAPISourceTest.java
@@ -3,17 +3,26 @@ package it.unipd.eis;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TheGuardianAPISourceTest {
-    private static final String FILE_PATH = "./Storage";
-    private Source source;
+    private static ArticleStorage articleStorage = new FileArticleStorage("./Storage/Test");
+    private final int numArticlesTest = 10;
+    private final String apiKeyPath = "./Storage/TheGuardianAPIKey.txt";
+    private TheGuardianAPISource source;
 
     @BeforeEach
     public void setup() {
-        source = new TheGuardianAPISource();
+        TheGuardianAPISource.setStorage(articleStorage);
+        source = new TheGuardianAPISource(apiKeyPath);
+        source.setNumArticles(numArticlesTest);
     }
 
     @Test
     public void testDownloadArticles() {
+        TheGuardianAPISource.clearStorage();
+        source.downloadArticles("nuclear power");
 
+        assertEquals(numArticlesTest, articleStorage.getArticleCount());
     }
 }
