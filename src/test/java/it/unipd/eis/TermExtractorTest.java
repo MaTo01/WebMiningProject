bce3125b720cc4d427cab32f8136ef7869cb2fcf
Van Sang Ho <vansang.hocorp@gmail.com>
2023-07-10 11:38:05 +0200

    test TermExtractor

--- src/test/java/it/unipd/eis/TermExtractorTest.java
+++ src/test/java/it/unipd/eis/TermExtractorTest.java
@@ -1,11 +1,34 @@
 package it.unipd.eis;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TermExtractorTest {
+    private final int numTermsTest = 50;
+    private static ArticleStorage articleStorage = new FileArticleStorage("./Storage/Test");
+    private static TermStorage termStorage = new FileTermStorage("./Storage/Test");
     private TermExtractor extractor;
+
     @BeforeEach
     public void setup() {
-        extractor = new TermExtractor();
+        extractor = new TermExtractor(numTermsTest);
+        extractor.setArticleStorage(articleStorage);
+        extractor.setTermStorage(termStorage);
+    }
+
+    @Test
+    public void testExtractTerms() {
+        termStorage.clearStorage();
+        Article article1 = new Article("Test 1", "The quick brown fox jumps over the lazy dog");
+        Article article2 = new Article("Test 2", "Now is the time for all good men to come to the aid of the party");
+        articleStorage.addArticle(article1);
+        articleStorage.addArticle(article2);
+        extractor.extractTerms();
+
+        assertEquals(14, termStorage.getTermCount());
+        assertTrue(termStorage.containsTerm(new Term("test", 2)));
     }
 }
