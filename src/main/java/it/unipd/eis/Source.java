aeb6c04f2c4c0a399cc16fd35d70ddc70bfa70c3
Van Sang Ho <vansang.hocorp@gmail.com>
2023-07-11 16:14:34 +0200

    finito javadoc Source

--- src/main/java/it/unipd/eis/Source.java
+++ src/main/java/it/unipd/eis/Source.java
@@ -9,10 +9,17 @@ public abstract class Source {
     protected ArrayList<Article> articles;
     protected static ArticleStorage articleStorage;
 
+    /**
+     * Default constructor, initializes the articles list.
+     */
     public Source() {
         articles = new ArrayList<>();
     }
 
+    /**
+     * Sets the ArticleStorage to be used by all Sources.
+     * @param as
+     */
     public static void setArticleStorage(ArticleStorage as) {
         articleStorage = as;
     }
