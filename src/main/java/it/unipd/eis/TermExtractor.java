37157fafa6c06971725270214c661bde75fb5f47
Van Sang Ho <vansang.hocorp@gmail.com>
2023-07-11 16:16:50 +0200

    finito javadoc TermExtractor, cambiato stoplistFilePath in una costante, cambiato la lista terms in una variabile d'istanza, aggiunto metodo serializeTerms() per ordinare di più extractTerms

--- src/main/java/it/unipd/eis/TermExtractor.java
+++ src/main/java/it/unipd/eis/TermExtractor.java
@@ -10,27 +10,44 @@ public class TermExtractor {
     private TermStorage termStorage;
     private ArticleStorage articleStorage;
     private final int MAX_TERMS_TO_SAVE;
+    private static final String STOPLIST_FILE_PATH = "Sources/stopList.txt";
     private static final ArrayList<String> STOPLIST = loadStopList();
+    private ArrayList<Term> terms;
 
+    /**
+     * Constructs a TermExtractor using the specified maximum number of terms to save.
+     * @param num the maximum number of terms to save
+     */
     public TermExtractor(int num) {
         MAX_TERMS_TO_SAVE = num;
+        terms = new ArrayList<>();
     }
 
+    /**
+     * Sets the ArticleStorage to be used by the TermExtractor.
+     * @param articleStorage the ArticleStorage to be used
+     */
     public void setArticleStorage(ArticleStorage articleStorage) {
         this.articleStorage = articleStorage;
     }
 
+    /**
+     * Sets the TermStorage to be used by the TermExtractor.
+     * @param termStorage the TermStorage to be used
+     */
     public void setTermStorage(TermStorage termStorage) {
         this.termStorage = termStorage;
     }
 
+    /**
+     * Extracts the top terms from the articles in the ArticleStorage and saves them using the TermStorage.
+     */
     public void extractTerms() {
         ArrayList<Article> articles = articleStorage.getAllArticles();
         if(articles == null || articles.size() == 0) {
             throw new IllegalStateException("Error retrieving articles (articles.json file may be missing or empty).");
         }
 
-        ArrayList<Term> terms = new ArrayList<>();
         for(Article a : articles) {
             String[] title = splitAndReformat(a.getTitle());
             String[] body = splitAndReformat(a.getBody());
@@ -44,7 +61,13 @@ public class TermExtractor {
         }
 
         Collections.sort(terms);
+        serializeTerms();
+    }
 
+    /**
+     * Serializes the top MAX_TERMS_TO_SAVE Terms
+     */
+    private void serializeTerms() {
         ArrayList<Term> topTerms = new ArrayList<>();
         for(int i = 0; i < MAX_TERMS_TO_SAVE && i < terms.size(); i++) {
             topTerms.add(terms.get(i));
@@ -52,6 +75,13 @@ public class TermExtractor {
         termStorage.addTerms(topTerms);
     }
 
+    /**
+     * Adds a previously not encountered Term into the terms list,
+     * increments its weight if it has already been encountered,
+     * or ignores it if it's in the stoplist
+     * @param terms the list of Terms currently found
+     * @param t the term in String format to be checked
+     */
     private void countTerm(ArrayList<Term> terms, String t) {
         if(!t.equals("") && !STOPLIST.contains(t.toLowerCase())) {
             Term aux = new Term(t);
@@ -65,13 +95,23 @@ public class TermExtractor {
         }
     }
 
+    /**
+     * Removes all punctuation and symbols from a String
+     * and splits it based on whitespaces into individual words
+     * @param s the String to be processed
+     * @return an array of Strings representing all the individual words
+     */
     private String[] splitAndReformat(String s) {
         s = s.replaceAll("[^a-zA-Z0-9\\s]", " ");
         return s.split(" ");
     }
 
+    /**
+     * Static method to read the stoplist from the specified text file
+     * @return an ArrayList containing the individual items of the stoplist
+     */
     private static ArrayList<String> loadStopList() {
-        try (BufferedReader reader = new BufferedReader(new FileReader("Sources/stopList.txt"))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(STOPLIST_FILE_PATH))) {
             String line;
             ArrayList<String> list = new ArrayList<>();
             while((line = reader.readLine()) != null) {
